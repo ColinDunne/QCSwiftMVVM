9cfd58e03d5811fcad42430e33183b87539f4a49
QianChen <xgdjqc@126.com>
2019-08-11 19:59:36 +0800

    创建新的工程

---
+++ QCSwiftMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QCSwiftMVVM
+//
+//  Created by Qian Chen on 2019/8/11.
+//  Copyright © 2019 Qian Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
